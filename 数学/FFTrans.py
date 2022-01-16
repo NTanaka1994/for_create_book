bbb7b2a5e4495cbc07bbde9c7999a3d3b4518416
Setsu-Hanatsuki <66735232+Setsu-Hanatsuki@users.noreply.github.com>
2022-01-16 21:25:00 +0900

    Add files via upload

---
+++ 数学/FFTrans.py
@@ -0,0 +1,17 @@
+import librosa
+import matplotlib.pyplot as plt
+import numpy as np
+
+x,sr=librosa.load("001-sibutomo.wav")
+mfcc=librosa.feature.mfcc(x,sr=sr)
+
+N=len(x)
+dt=1/sr
+F=np.fft.fft(x)
+freq=np.linspace(0,sr,N)
+amp=np.abs(F)
+plt.bar(freq,amp)
+plt.xlim(0,max(freq)/2)
+plt.savefig("powerspectol.png")
+plt.clf()
+print("Complete!")
---
+++ 数学/noise_filter.py
@@ -0,0 +1,21 @@
+import numpy as np
+import librosa
+from scipy.io.wavfile import write
+
+x,sr=librosa.load("001-sibutomo.wav")
+N=len(x)
+freq=np.linspace(0,sr,N)
+F=np.fft.fft(x)
+
+F=F/(N/2)
+F[0]=F[0]/2
+
+F2=F.copy()
+
+F2[(freq>5000)]=0
+F2[(freq<50)]=0
+
+x2=np.fft.ifft(F2)
+x2=np.real(x2*N)
+
+write("filterd.wav",rate=sr,data=x2)
