5dfe1aa459c0e075159dc98ee7a3ab4821d0e068
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2024-03-16 23:34:41 +0900

    Update trans2wav.py

--- 数学/trans2wav.py
+++ 数学/trans2wav.py
@@ -1,7 +1,7 @@
 from pydub import AudioSegment as AS
 
-src="g_23"
-dst="g_23.wav"
+src = "g_23"
+dst = "g_23.wav"
 
-audSeg=AS.from_mp3(src+".mp3")
-audSeg.export(dst,format="wav")
+audSeg = AS.from_mp3(src + ".mp3")
+audSeg.export(dst, format="wav")
