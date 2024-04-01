68285967bfe668a5f7014c61fabb10f2d559d96f
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2024-04-01 23:10:48 +0900

    Update detA.py

--- 画像編集/detA.py
+++ 画像編集/detA.py
@@ -2,12 +2,12 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-pic=Image.open("sample.png")
-pic=pic.convert(mode="L")
-pic=np.array(pic)
-detp=np.linalg.pinv(pic)
-plt.imshow(detp,cmap="gray")
+pic = Image.open("sample.png")
+pic = pic.convert(mode="L")
+pic = np.array(pic)
+detp = np.linalg.pinv(pic)
+plt.imshow(detp, cmap="gray")
 plt.show()
 
-plt.imshow(np.dot(pic,detp),cmap="gray")
+plt.imshow(np.dot(pic, detp), cmap="gray")
 plt.show()
