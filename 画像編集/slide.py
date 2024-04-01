911c125e7de7010ea60721e5cb2d2420ec5c0a96
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2024-04-01 23:21:39 +0900

    Update slide.py

--- 画像編集/slide.py
+++ 画像編集/slide.py
@@ -2,20 +2,20 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-pic=Image.open("sample.png")
-pic=pic.convert(mode="L")
-pic=np.array(pic)
-h,w=pic.shape[0],pic.shape[1]
+pic = Image.open("sample.png")
+pic = pic.convert(mode="L")
+pic = np.array(pic)
+h, w = pic.shape[0], pic.shape[1]
 
 #拡大行列
-sy=-100#縦100移動
-sx=20#横20移動
-dst=np.zeros((h+abs(sy),w+abs(sx)))
+sy = - 100#縦100移動
+sx = 20#横20移動
+dst = np.zeros((h+abs(sy), w+abs(sx)))
 for y in range(h):
     for x in range(w):
-        xi=int((x)+sx)
-        yi=int((y)+sy)
-        dst[yi][xi]=pic[y][x]
-plt.imshow(dst,cmap="gray")
+        xi = int((x)+sx)
+        yi = int((y)+sy)
+        dst[yi][xi] = pic[y][x]
+plt.imshow(dst, cmap="gray")
 plt.savefig("expand-1-1.png")
 plt.show()
